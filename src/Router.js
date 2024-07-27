1f62c5bab707b23f263c7ea7696cf9cd760981db
Vivek <67629894+vivekpaul-exe@users.noreply.github.com>
2024-07-28 01:38:50 +0530

    Update Router.js
    Updated description in infopage

--- src/Router.js
+++ src/Router.js
@@ -7,7 +7,7 @@ import StickyFooter from "./components/Footer"
 import Navbar from "./components/NavBar/Navbar"
 import Home from "./components/Home"
 import { CuisinePage } from "./components/Pages/CuisinePage"
-import { InfoPage} from "./components/Pages/InfoPage"
+//import { InfoPage} from "./components/Pages/InfoPage"
 import   RecipePage  from "./components/Pages/RecipePage"
 import {WinePage} from "./components/Pages/Wines"
 import  { ItemList } from "./components/Pages/ItemList"
@@ -22,7 +22,7 @@ export default function RouteConfig(){
              <Routes>
                 <Route path="/" element={<Develop />} />
                 
-                <Route path="/info" element={<InfoPage />} />
+        //        <Route path="/info" element={<InfoPage />} />
                 <Route path="/ingredients" element={<IngredientsPage />} />
                 <Route path="/recipe/:id" element={<RecipePage />} />
                 <Route path="/Cuisines/:type" element={<CuisinePage />} />
